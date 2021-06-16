aa5a330769af9a334a6d0fff9644acf705abd48c
Thomas Maxey <tmaxey201@gmail.com>
2021-06-16 15:57:36 -0400

    added apiroute coding

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const { v4: uuidv4 } = require('uuid');
 
 
 module.exports = (app) => {
@@ -8,6 +9,22 @@ module.exports = (app) => {
   });
   app.get("/api/notes/:id", (req, res) => {
     res.json(data[Number(req.params.id)]);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    let note = req.body;
+    let uniqueID = data.length;
+    console.log(uniqueID);
+    note.id = uniqueID;
+    data.push(note);
+    req.body.id = uuidv4();
+
+
+    fs.writeFileSync("../db/db.json", JSON.stringify(data), (err) => {
+      if (err) console.log(err);
+    });
+
+    res.json(data);
   });
 
 }
